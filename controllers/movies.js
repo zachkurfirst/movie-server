566abcf22f4816409e27acd34e111b7681476d26
Zach Kurfirst <zachmk11@gmail.com>
2023-12-15 13:44:14 -0500

    index functionality for top rated movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,47 +1,27 @@
-const token = process.env.TMDB_TOKEN;
 const ROOT_URL = "https://api.themoviedb.org/3";
-// const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 
 async function index(req, res) {
   try {
-    let endpoint = `${ROOT_URL}/movie/top_rated?language=en-US&page=1&api_key=${token}`;
+    let endpoint = `${ROOT_URL}/movie/top_rated?language=en-US&page=1`;
     console.log(endpoint);
-    const res = await fetch(endpoint, {
+    const movieDbResponse = await fetch(endpoint, {
       method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZGM4Y2RmNzljN2U3NmY5YmFlNjAwZjNiNTZmNjUwYiIsInN1YiI6IjY1N2M3ZWIzMTc2YTk0MTczMDYyMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eJTxjvV3CWQ3I7eTyWWAXnDK2ncyVz_RqrJLqU86yBw",
+      },
     });
-    //   headers: {
-    //     accept: "application/json",
-    //     Authorization:
-    //       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZGM4Y2RmNzljN2U3NmY5YmFlNjAwZjNiNTZmNjUwYiIsInN1YiI6IjY1N2M3ZWIzMTc2YTk0MTczMDYyMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eJTxjvV3CWQ3I7eTyWWAXnDK2ncyVz_RqrJLqU86yBw",
-    //   },
-    // });
-    if (res.ok) {
-      //   console.log(res.json());
-      console.log("working");
-      return res.json();
-      // res.send(json);
-      //   return "Working"
-    }
+    console.log(movieDbResponse);
+    // res.send("hitting endpoint");
+    const data = await movieDbResponse.json();
+    res.json({ message: "api response", data });
   } catch (err) {
     console.log(err);
+    res.json({ message: "error", error: res.statusText });
   }
 }
-
-//   let endpoint = `${ROOT_URL}/movie/top_rated?language=en-US&page=1&api_key=${token}`;
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       accept: "application/json",
-//       Authorization:
-//         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZGM4Y2RmNzljN2U3NmY5YmFlNjAwZjNiNTZmNjUwYiIsInN1YiI6IjY1N2M3ZWIzMTc2YTk0MTczMDYyMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eJTxjvV3CWQ3I7eTyWWAXnDK2ncyVz_RqrJLqU86yBw",
-//     },
-//   };
-
-//   fetch(endpoint, options)
-//     .then((res) => res.json())
-//     .then((json) => console.log(json))
-//     .catch((err) => console.error("error:" + err));
-// }
 
 module.exports = {
   index,
